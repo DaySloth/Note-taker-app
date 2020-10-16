1cdf64a601869fbad7dd35f165fae4520c2a2ae7
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-16 13:16:42 -0500

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 const renderHtml = function(app){
-    app.get("*", function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/notes", function(req, res){
-        console.log("getting '/notes'")
+        console.log("getting '/notes'");
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 };
