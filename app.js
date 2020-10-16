ad3b54d8da5edfcafe19521ec2cf14511ab2fcd5
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-10-15 21:28:10 -0500

    created app listening and route calling

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+htmlRoutes(app);
+apiRoutes(app);
+
+app.listen(PORT, function(){
+    console.log("Server listening on port: " + PORT);
+});
